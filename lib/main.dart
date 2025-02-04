cf48cfd499a9bf3a09676e2e0b09ec73a3a17c2d
Utkarsh-Jha-codes <utkarshjhafeb2006@gmail.com>
2025-02-04 15:43:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: Homepage(),
     );
   }
 }
