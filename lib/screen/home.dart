f912a2d832cd7f56927cbc0a52e08ebf14b735cc
Utkarsh-Jha-codes <utkarshjhafeb2006@gmail.com>
2025-02-04 15:41:08 +0530

    Update home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,16 +1,36 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
 
+
+class Homepage extends StatefulWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomepageState createState() => _HomepageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users =  [];
+
+class _HomepageState extends State<Homepage> {
+  List < Map < String, String >> articles = [];
+  @override
+  void initState() {
+    super.initState();
+    apicall();
+  }
+  Future<void> apicall() async {
+    http.Response response;
+    response = await http.get(Uri.parse("https://newsapi.org/v2/everything?q=tesla&from=2025-01-05&sortBy=publishedAt&apiKey=3e0e11c8734549b9b2ffd86ff48fc9ee"));
+    if (response.statusCode == 200) {
+      setState(() {
+        var jsonResponse = jsonDecode(response.body);
+        var articlesList = jsonResponse["articles"];
+        articles = articlesList.map<Map<String, String>> ((articles) => {
+          'title': articles['title'].toString(),
+          'description': articles['description'].toString()
+        }).toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,32 +38,26 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blueAccent,
         title: Center(child: Text('Information'),),
       ),
-      body: ListView.builder(
-        itemCount: users.length,
-          itemBuilder: (context,index){
-          var user = users[index];
-          final email = user['email'];
-        return ListTile(
-        title: Text(email),
-        );
-      }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
-      ),
+      body: Center(
+        child: ListView.builder(
+    itemCount: articles.length,
+    itemBuilder: (context, index) {
+      return Card (
+        margin: EdgeInsets.all(10),
+    child: ListTile(
+    title: Text(
+    articles[index]['title'] ?? 'No Tittle',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text(
+    articles[index]['description'] ?? 'No Description',
+    textAlign: TextAlign.justify,
+    ),
+    ),
+      );
+    },
+    ),
+    ),
     );
-  }
-
-
-  void fetchUsers() async {
-    print("fetchUsers called");
-    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-12-27&sortBy=publishedAt&apiKey=3e0e11c8734549b9b2ffd86ff48fc9ee ';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    setState(() {
-      users = json['results'];
-    });
-    print ('fetchUsers completed');
   }
 }
